e37149bfeaa75015640e93fc7af8b66dfb614be0
Amila <sales@blackcarbonfeast.com>
2019-06-23 23:53:56 +0530

    Initial commit

---
+++ app/src/main/java/com/playapplication/tipstrickstech/Activities/Register.java
@@ -0,0 +1,15 @@
+package com.playapplication.tipstrickstech.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.playapplication.tipstrickstech.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
