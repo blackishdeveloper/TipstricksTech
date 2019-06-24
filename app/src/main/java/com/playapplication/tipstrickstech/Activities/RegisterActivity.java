e79adde429fcba4016471ece34e07521ec6578c6
Amila <sales@blackcarbonfeast.com>
2019-06-24 20:05:48 +0530

    add codes

---
+++ app/src/main/java/com/playapplication/tipstrickstech/Activities/RegisterActivity.java
@@ -0,0 +1,75 @@
+package com.playapplication.tipstrickstech.Activities;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.playapplication.tipstrickstech.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    ImageView ImgUserPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        //img view
+
+        ImgUserPhoto = findViewById(R.id.regUserProfile);
+
+        ImgUserPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (Build.VERSION.SDK_INT >= 22) {
+
+                    checkAndRequestForPermission();
+
+
+                } else {
+                    openGallery();
+                }
+            }
+        });
+
+    }
+
+    private void openGallery() {
+
+        //Todo open gallery intent and wait for user to pick an image
+
+
+    }
+
+    private void checkAndRequestForPermission() {
+
+        if (ContextCompat.checkSelfPermission(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(RegisterActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+
+                Toast.makeText(RegisterActivity.this, "Please accept for the Permission", Toast.LENGTH_SHORT).show();
+            }
+
+
+        } else {
+            ActivityCompat.requestPermissions(RegisterActivity.this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    PregCode);
+        }
+
+
+    }
+
+}
+
+
